a22951e153d7f2e10b297f595a58a42b05c9c659
DanBadRobot <danmikhael@gmail.com>
2018-07-25 08:56:12 +1000

    fCC Basic Javascript lessons - finding nth to last character in a string

---
+++ fCC-basic-javascript-bracket-notation-nth-to-last.js
@@ -0,0 +1,7 @@
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+var lastName = "Lovelace";
+
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
