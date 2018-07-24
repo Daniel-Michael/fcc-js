a83edb52943a695dcd89660144db8270d514326e
DanBadRobot <danmikhael@gmail.com>
2018-07-25 09:55:42 +1000

    fCC Basic Javascript lessons - manipulate arrays with unshift

---
+++ fCC-basic-javascript-manipulate-arrays-with-unshift.js
@@ -0,0 +1,8 @@
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy"); 
+
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+myArray.unshift(["Paul", 35]);
