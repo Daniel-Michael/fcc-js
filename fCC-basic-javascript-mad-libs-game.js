fb0d81b600dde3225622fcce63d624d0e743cf53
DanBadRobot <danmikhael@gmail.com>
2018-07-25 09:14:10 +1000

    fCC Basic Javascript lessons - creating a mad libs style game with word blanks

---
+++ fCC-basic-javascript-mad-libs-game.js
@@ -0,0 +1,8 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+    result ="Defying the way it looked, the "+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb+"!";
+  return result;
+}
+
+
+wordBlanks("dog", "big", "ran", "quickly");s
