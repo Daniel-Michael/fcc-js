49c70de5da014c3daeb8e9c04416af9f11e0b5c5
DanBadRobot <danmikhael@gmail.com>
2018-07-24 11:50:37 +1000

    fCC Basic Javascript - constructing strings with concatenation of variables

---
+++ fCC-basic-javascript-constructing-strings.js
@@ -0,0 +1,6 @@
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+var myName ="Daniel Michael. ";
+var myStr = "My name is " + myName + ", and I am well!";
+
