44642a9e8beda4f480796ec6899f1a8b3557730c
DanBadRobot <danmikhael@gmail.com>
2018-07-24 11:54:03 +1000

    fCC Basic Javascript - append variables to strings

---
+++ fCC-basic-javascript-append-variables-strings.js
@@ -0,0 +1,7 @@
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+var someAdjective = "great!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
