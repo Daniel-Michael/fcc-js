ceab410d07c4c3d8e6c5b3e104d266596edb8ca4
DanBadRobot <danmikhael@gmail.com>
2018-07-26 11:26:03 +1000

    understanding undefined value returned from a function

---
+++ fCC-basic-javascript-functions-with-undefined-values.js
@@ -0,0 +1,14 @@
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+console.log(minusSeven(10));
+
+// my code
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(5));
