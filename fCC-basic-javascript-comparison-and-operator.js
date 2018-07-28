a715904ce831d361e896fadb21a12220143a5fdb
DanBadRobot <danmikhael@gmail.com>
2018-07-28 21:36:55 +1000

    freeCodeCamp Basic Javascript lessons - comparison and operator

---
+++ fCC-basic-javascript-comparison-and-operator.js
@@ -0,0 +1,11 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+    } else {
+    return "No";
+  }
+};
+// Change this value to test
+testLogicalAnd(24);
