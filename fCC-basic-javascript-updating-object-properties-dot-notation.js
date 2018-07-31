dabea97d9584d8f56677d924a41b380c6cf1f0aa
DanBadRobot <danmikhael@gmail.com>
2018-07-31 16:12:38 +1000

    freeCodeCamp Basic Javascript lessons - updating object properties using dot notation

---
+++ fCC-basic-javascript-updating-object-properties-dot-notation.js
@@ -0,0 +1,21 @@
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Updating myDog name variable using dot notation
+
+myDog.name = "Happy Coder";
