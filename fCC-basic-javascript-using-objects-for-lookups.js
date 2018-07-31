2e0cf953daf8ddd787f83fa8482f7855b15fb03d
DanBadRobot <danmikhael@gmail.com>
2018-07-31 18:59:35 +1000

    freeCodeCamp Basic Javascript lessons - using objects for lookups

---
+++ fCC-basic-javascript-using-objects-for-lookups.js
@@ -0,0 +1,14 @@
+function phoneticLookup(val) {
+  var result = "";
+  var lookup = {
+  "alpha": "Adams",
+  "bravo": "Boston",
+  "charlie": "Chicago",
+  "delta": "Denver",
+  "echo": "Easy",
+  "foxtrot": "Frank",
+  };
+  return lookup[val]
+  return result;
+}
+phoneticLookup("charlie");
