cd12895b1b5d4bd2ffa60a78ace1f2f86e562aca
DanBadRobot <danmikhael@gmail.com>
2018-07-31 16:22:57 +1000

    freeCodeCamp Basic Javascript lessons - adding new properties to a JS object

---
+++ fCC-basic-javascript-add-new-properties-to-an-object.js
@@ -0,0 +1,14 @@
+ourDog.bark = "bow-wow";
+
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// adding a property to the myDog variable with dot notation
+myDog.bark = "woof";
+
+// adding a further property using bracket notation
+myDog.smell = "funny";
