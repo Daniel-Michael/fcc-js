58572fd4bfadd3e2323533d9d79593e34d8c2d40
DanBadRobot <danmikhael@gmail.com>
2018-08-01 11:03:09 +1000

    freeCodeCamp Basic Javascript lessons - manipulating complext objects creating album lists

---
+++ fCC-basic-javascript-manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+ "artist": "Andrew Bayer",
+    "title": "If it were you",
+    "release_year": 2016,
+    "formats": [ 
+      "CD",
+      "MP3",
+      "LP"
+    ],
+    "gold": false
+  }
+];
