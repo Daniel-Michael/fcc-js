df3e76a08b0232662150e3d8bdc27586d2080364
Dan Michael <danmikhael@gmail.com>
2018-08-18 14:36:49 +1000

    Generate whole numbers within a range

---
+++ generate-whole-numbers-in-a-range.js
@@ -0,0 +1,15 @@
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; + myMin;
+}
+
+randomRange (2,8);
+
+var myRandom = randomRange(5, 15);
